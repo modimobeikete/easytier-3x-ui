419b08b04984c72ccd27d54b4ca8f64cb19a81c5
modimobeikete <1244176069@qq.com>
2025-07-03 12:02:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -44,5 +44,5 @@ if ! systemctl restart x-ui; then
 fi
 
 echo "x-ui installation completed successfully"
-/opt/easytier/easytier-core -w ${EASYTIERWEB_USERNAME} --machine-id ${MACHINE_ID} ${EXTRA_ARGS}
+./easytier-core -w ${EASYTIERWEB_USERNAME} --machine-id ${MACHINE_ID} ${EXTRA_ARGS}
 wait
